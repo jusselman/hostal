b1b0277571e2de19d51bc09472438ddbd0399d64
Joshua Usselman <usselmanjosh@gmail.com>
2018-09-22 20:58:17 -0700

    reservation section

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,3 +3,26 @@ $(document).ready(function() {
     $('.nav-responsive').toggleClass('show');
   });
 });
+
+
+
+// RESERVATION SECTION //
+(function($){
+	function floatLabel(inputType){
+		$(inputType).each(function(){
+			var $this = $(this);
+			// on focus add cladd active to label
+			$this.focus(function(){
+				$this.next().addClass("active");
+			});
+			//on blur check field and remove class if needed
+			$this.blur(function(){
+				if($this.val() === '' || $this.val() === 'blank'){
+					$this.next().removeClass();
+				}
+			});
+		});
+	}
+	// just add a class of "floatLabel to the input field!"
+	floatLabel(".floatLabel");
+})(jQuery);
