ee483d32305f6023a15b9a47bc9ad1bdff84dde8
Joshua Usselman <usselmanjosh@gmail.com>
2018-09-24 23:44:23 -0700

    carousel revision

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,3 +26,106 @@ $(document).ready(function() {
 	// just add a class of "floatLabel to the input field!"
 	floatLabel(".floatLabel");
 })(jQuery);
+
+
+
+// SMOOTH SCROLLING JQUERY //
+//
+// $(document).ready(function() {
+//
+//   var scrollLink = $('.scroll');
+//
+//   // Smooth scrolling
+//   scrollLink.click(function(event) {
+//     event.preventDefault();
+//     $('body,html').animate({
+//       scrollTop: $(this.hash).offset().top
+//     }, 1250 );
+//   });
+//
+//   // Active link switching
+//   $(window).scroll(function() {
+//     var scrollbarLocation = $(this).scrollTop();
+//
+//     scrollLink.each(function() {
+//
+//       var sectionOffset = $(this.hash).offset().top;
+//
+//       if ( sectionOffset <= scrollbarLocation ) {
+//         $(this).parent().addClass('active');
+//         $(this).parent().addClass('active::before');
+//         $(this).parent().siblings().removeClass('active');
+//         $(this).parent().siblings().removeClass('active::before');
+//       }
+//     })
+//   })
+// })
+
+
+// NAVBAR CHANGE //
+
+
+
+// SMOOTH SCROLLING JQUERY //
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 1250, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
+
+// $(document).ready(function() {
+//
+//   var scrollLink = $('.scroll');
+//
+//   // Smooth scrolling
+//   scrollLink.click(function(event) {
+//     event.preventDefault();
+//     $('body,html').animate({
+//       scrollTop: $(this.hash).offset().top
+//     }, 1250 );
+//   });
+//
+// });
+//
+// // active link switch //
+//
+// $(window).scroll(function() {
+//   var scrollbarLocation = $(this).scrollTop();
+//
+//   scrollLink.each(function() {
+//
+//     var sectionOffset = $(this.hash).offset().scrollTop
+//     if (sectionOffset <= scrollbarLocation) {
+//       $(this).parent().addClass('active');
+//       $(this).parent().siblings().removeClass('active');
+//     }
+//   })
+// });
+
+
+// Change navbar on scroll //
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+      if(scroll>80) {
+        $('#nav').css('height', '60px');
+        $('#nav').css('width', '100%');
+        $('#mainNav').css('margin', "-10px auto");
+      }
+      else {
+        $('#nav').css('height', '80px');
+        $('#nav').css('width', '60%');
+        $('#mainNav').css('margin', "25px auto");
+
+
+      }
+  })
+})
